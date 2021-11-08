78d87abef7a1e42642d4e712d6c8dca4cf214eaa
jujunjuna <jujunn.0698@gmail.com>
2021-11-08 11:00:56 +0700

    program mencari bilangan terbesar

---
+++ mencari_terbesar.py
@@ -0,0 +1,7 @@
+a = int(input("masukan bilanngan peratama: "))
+b = int(input("masukan bilangan kedua: "))
+
+if a > b :
+    print (a,"adalah bilangan terbesar")
+else:
+    print(b,"adalah bilangan terbesar")
